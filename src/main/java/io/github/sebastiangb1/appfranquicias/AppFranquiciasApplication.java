a2fbe34eba74e2dcc6547d94334b5c53157f2bff
Sebastian Giraldo <79490763+SebastianGB1@users.noreply.github.com>
2025-02-18 16:12:36 -0500

    init

---
+++ src/main/java/io/github/sebastiangb1/appfranquicias/AppFranquiciasApplication.java
@@ -0,0 +1,13 @@
+package io.github.sebastiangb1.appfranquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFranquiciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFranquiciasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sebastiangb1/appfranquicias/AppFranquiciasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sebastiangb1.appfranquicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFranquiciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
