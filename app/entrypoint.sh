04dc4152d30ab7e7a1544ee8426981b425ff8915
Sebastian Giraldo <79490763+SebastianGB1@users.noreply.github.com>
2025-02-19 22:28:13 -0500

    add docker entrypoint

---
+++ app/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+./mvnw package
+
+
+exec java -jar \
+    -DDB_NAME="${DB_NAME:-franquicias}" \
+    -DDB_HOST="${DB_HOST:-localhost}" \
+    -DDB_PORT="${DB_PORT:-3306}" \
+    -DDB_USER="${DB_USER:-root}" \
+    -DDB_PASSWORD="${DB_PASSWORD}" \
+    target/app-franquicias-0.0.1-SNAPSHOT.jar
